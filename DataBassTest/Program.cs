59a7958efeda5facb5ef48d41fd56d9c70148637
novakvova <novakvova@gmail.com>
2022-06-27 16:19:54 +0300

    BulkCopy

--- DataBassTest/Program.cs
+++ DataBassTest/Program.cs
@@ -3,6 +3,10 @@ using Microsoft.Extensions.Configuration;
 using System.Text;
 using System.IO;
 using System.Data.SqlClient;
+using Bogus;
+using System.Diagnostics;
+using System.Collections.Generic;
+using System.Data;
 
 namespace DataBassTest
 {
@@ -22,18 +26,75 @@ namespace DataBassTest
 
             //Console.WriteLine("Вкажіть назву таблиці");
             //string tableName = Console.ReadLine();
-            SqlConnection con = new SqlConnection(connection);
-            con.Open();
+            //SqlConnection con = new SqlConnection(connection);
+            //con.Open();
 
-            SqlCommand cmd = con.CreateCommand();
-            cmd.CommandText = "SELECT name FROM master.sys.databases";
-            using (var reader = cmd.ExecuteReader())
+            //SqlCommand cmd = con.CreateCommand();
+            //cmd.CommandText = "SELECT name FROM master.sys.databases";
+            //using (var reader = cmd.ExecuteReader())
+            //{
+            //    while (reader.Read())
+            //    {
+            //        Console.WriteLine(reader["name"]);
+            //    }
+            //}
+
+            //Console.WriteLine("вказіть БД для роботи: ");
+            Stopwatch stopWatch = new Stopwatch();
+            stopWatch.Start();
+            string dbName = "pd123";//Console.ReadLine();
+            SqlConnection con = new SqlConnection(connection+ $"Initial Catalog={dbName}");
+            con.Open();
+            Console.WriteLine("Вкажіть кількість записів:");
+            int n = int.Parse(Console.ReadLine());
+            var faker = new Faker<UserCreate>("uk")
+                .RuleFor(x => x.Name, f => f.Person.FullName)
+                .RuleFor(x => x.Email, f => f.Internet.Email())
+                .RuleFor(x => x.Phome, f => f.Phone.PhoneNumber());
+            List<UserCreate> users = new List<UserCreate>();
+            for (int i = 0; i < 100; i++)
             {
-                while (reader.Read())
-                {
-                    Console.WriteLine(reader["name"]);
-                }
+                var user = faker.Generate();
+                
+
+                users.Add(user);
             }
+
+            DataTable dt = new DataTable();
+            dt.Columns.Add(new DataColumn(nameof(UserCreate.Id)));
+            dt.Columns.Add(new DataColumn(nameof(UserCreate.Email)));
+            dt.Columns.Add(new DataColumn(nameof(UserCreate.Name)));
+            dt.Columns.Add(new DataColumn(nameof(UserCreate.Phome)));
+            Random random = new Random();
+            for (int i = 0; i < n; i++)
+            {
+                int r = random.Next(0, 100);
+                var user = users[r];
+                DataRow row = dt.NewRow();
+                row[nameof(UserCreate.Id)]=user.Id;
+                row[nameof(UserCreate.Email)]=user.Email;
+                row[nameof(UserCreate.Name)]=user.Name;
+                row[nameof(UserCreate.Phome)]=user.Phome;
+                dt.Rows.Add(row);
+                //SqlCommand cmd = con.CreateCommand();
+                //cmd.CommandText = "INSERT INTO tblUsers " +
+                //    "(Email, Name, Phome) " +
+                //    $"VALUES('{user.Email}', N'{user.Name}', '{user.Phome}'); ";
+                //cmd.ExecuteNonQuery();
+            }
+            using(SqlBulkCopy bulkCopy = new SqlBulkCopy(con))
+            {
+                bulkCopy.DestinationTableName = "tblUsers";
+                bulkCopy.WriteToServer(dt);
+            }
+            stopWatch.Stop();
+            TimeSpan ts = stopWatch.Elapsed;
+            // Format and display the TimeSpan value.
+            string elapsedTime = String.Format("{0:00}:{1:00}:{2:00}.{3:00}",
+                ts.Hours, ts.Minutes, ts.Seconds,
+                ts.Milliseconds / 10);
+            Console.WriteLine("RunTime " + elapsedTime);
+
             //cmd.CommandText = "INSERT INTO users " +
             //    "(Email, FirstName, LastName) " +
             //    "VALUES('jj@ff.ffu', N'Іван', N'Драч'); ";
---
+++ DataBassTest/User.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataBassTest
+{
+    public class UserCreate
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Phome { get; set; }
+    }
+}
