ca53bb8f133971fa9c6adc840b137da7d5b4e692
novakvova <novakvova@gmail.com>
2022-07-04 16:21:50 +0300

    Add SQL Query join

---
+++ WorkingDatabase/MainForm.Designer.cs
@@ -0,0 +1,63 @@
+﻿namespace WorkingDatabase
+{
+    partial class MainForm
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnGenTabels = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnGenTabels
+            // 
+            this.btnGenTabels.Location = new System.Drawing.Point(641, 32);
+            this.btnGenTabels.Margin = new System.Windows.Forms.Padding(4);
+            this.btnGenTabels.Name = "btnGenTabels";
+            this.btnGenTabels.Size = new System.Drawing.Size(184, 74);
+            this.btnGenTabels.TabIndex = 0;
+            this.btnGenTabels.Text = "Генерація таблиць в БД";
+            this.btnGenTabels.UseVisualStyleBackColor = true;
+            this.btnGenTabels.Click += new System.EventHandler(this.btnGenTabels_Click);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(11F, 28F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(854, 423);
+            this.Controls.Add(this.btnGenTabels);
+            this.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "MainForm";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private Button btnGenTabels;
+    }
+}
---
+++ WorkingDatabase/MainForm.cs
@@ -0,0 +1,47 @@
+using System.Data.SqlClient;
+
+namespace WorkingDatabase
+{
+    public partial class MainForm : Form
+    {
+        SqlConnection con;
+        string dirSql = "SqlTabels";
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void btnGenTabels_Click(object sender, EventArgs e)
+        {
+            string dbName = "myPD123";
+            string conSTR = "Data Source=.;Integrated Security=True;";
+            string conectionSTR = $"{conSTR}Initial Catalog={dbName}";
+            con = new SqlConnection(conectionSTR);
+            con.Open();
+
+            CreateTabels();
+        }
+        private void CreateTabels()
+        {
+            ExecuteCommandFromFile("tblRegions.sql");
+            ExecuteCommandFromFile("tblCities.sql");
+            ExecuteCommandFromFile("tblUsers.sql");
+            ExecuteCommandFromFile("tblRoles.sql");
+            ExecuteCommandFromFile("tblUserRoles.sql");
+            ExecuteCommandFromFile("tblUserAdresses.sql");
+        }
+
+        private void ExecuteCommandFromFile(string file)
+        {
+            string sql = ReadSqlFile(file);
+            SqlCommand command = con.CreateCommand();
+            command.CommandText = sql;
+            command.ExecuteNonQuery(); //посилаємо команду в БД
+        }
+        private string ReadSqlFile(string file)
+        {
+            string sql = File.ReadAllText($"{dirSql}\\{file}");
+            return sql;
+        }
+    }
+}
